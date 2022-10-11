3aff76424be67eb8b57fb26ce122150b38dead49
Василий Виссаров <vissar2016@rambler.ru>
2022-10-11 23:20:45 +0300

    program to find the largest number added

---
+++ Example_008_Massiv/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numbers = {1, 8, 3, 2, 6};
+int i = 0;
+int max = numbers [0];
+int size = 5;
+
+while (i < size)
+{
+    if (numbers[i] > max)
+    {
+    max = numbers[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
