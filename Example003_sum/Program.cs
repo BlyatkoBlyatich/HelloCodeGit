8d65e106065f14561bd872483836b0c657c18194
Василий Виссаров <vissar2016@rambler.ru>
2022-10-11 20:42:27 +0300

    add Random

---
+++ Example003_sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10); // случайное число 1, 2, 3, 9 - максимальное число
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); // 1-10
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine (result);
