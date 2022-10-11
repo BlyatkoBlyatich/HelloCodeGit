e3b0ddbd5d8944f985b6f94d1c2000e8122a43e1
Василий Виссаров <vissar2016@rambler.ru>
2022-10-12 00:22:22 +0300

    Устранил недостатки программы Example 11, добавил возможность ввода массива любого значения чисел

---
+++ Example_12_SecondLargeNumReadLine/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//int [] numbers = {Console.ReadLine()}; 
+int size = numbers.Length, current_i = 0,
+max_number_i = 0, max = 0,
+temp = 0;
+
+while (current_i < size)
+{
+    if (numbers[current_i]>max)
+    {
+        temp = max;
+        max = numbers[current_i];
+        max_number_i = current_i;
+    }
+    else
+    {
+        if (numbers[current_i] > temp)
+        {
+            temp = numbers[current_i];
+        }
+           
+    }
+    current_i = current_i + 1;
+}
+Console.WriteLine (max);
+Console.WriteLine (temp);
