373deb1d69960a78f17499cd64dc91b59852d6fc
Василий Виссаров <vissar2016@rambler.ru>
2022-10-11 23:49:03 +0300

    Нахождение максимального числа в массиве

---
+++ Example_10_twoOfLargesNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] numbers = {3, 20, 18, 103, 92};
+int size = 5, current_i = 0,
+max_number_i = 0, max = numbers[0];
+
+while (current_i < size)
+{
+    if (numbers[current_i]>max)
+    {
+        max = numbers[current_i];
+        max_number_i = current_i;
+    }
+    current_i = current_i + 1;
+}
+Console.WriteLine (max);
+Console.WriteLine (current_i);
