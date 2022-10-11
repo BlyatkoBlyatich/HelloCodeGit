1045b14bf91aeb4deca4557faf8b16d3c7faaf56
Василий Виссаров <vissar2016@rambler.ru>
2022-10-11 23:19:12 +0300

    added ScalarProd programm

---
+++ Example_009_SclarProd/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] firstVector = {6,6};
+int [] secondVector = {6,6};
+int ScalarProd = 0;
+int i = 0;
+int size = 2;
+
+while (i<size)
+{
+    ScalarProd = ScalarProd+firstVector[i]*secondVector[i];
+    i = i + 1;
+}
+Console.WriteLine (ScalarProd);
