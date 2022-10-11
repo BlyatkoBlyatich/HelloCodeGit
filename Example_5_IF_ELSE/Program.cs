b6dee59d0de04dcbbd705f5f4e96d6b68d6b030b
Василий Виссаров <vissar2016@rambler.ru>
2022-10-11 20:52:52 +0300

    add Example5: if_else

---
+++ Example_5_IF_ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "василиса")
+{
+    Console.WriteLine("Ура, это же Василиса!");
+}
+else
+{   
+    Console.Write("Привет, полупокер по имени ");
+    Console.WriteLine(username);
+}
