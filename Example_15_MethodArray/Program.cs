a11bc7c28bbe52d788939e2e4d22323cf35270c4
Василий Виссаров <vissar2016@rambler.ru>
2022-10-16 23:24:23 +0300

    Find index of number

---
+++ Example_15_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {12,63,45,62,7189,42,566, 566};
+
+int n = array.Length; // возвращает количество элементов массива
+int find = 566;
+
+int index = 0; //счетчик индекса
+
+while(index < n)
+{
+    
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
